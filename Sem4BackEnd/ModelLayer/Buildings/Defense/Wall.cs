d9f71b679cba7646c510bca7d4e95dee245a9bef
Simonqwadjke <Simonqwadjke@gmail.com>
2016-04-01 15:25:24 +0200

    Added all model layer classes

--- Sem4BackEnd/ModelLayer/Buildings/Defense/Wall.cs
+++ Sem4BackEnd/ModelLayer/Buildings/Defense/Wall.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ModelLayer
-{
-    public class Class1
-    {
+namespace ModelLayer.Buildings.Defense {
+    class Wall {
     }
 }
