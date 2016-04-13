581d2913803e52a2f59c3cd3c9a980c4b9fc0449
Simonqwadjke <Simonqwadjke@gmail.com>
2016-04-13 19:05:11 +0200

    Rettelse i database modellen

--- Sem4BackEnd/ModelLayer/User.cs
+++ Sem4BackEnd/ModelLayer/User.cs
@@ -7,20 +7,56 @@ using ModelLayer.Units;
 
 namespace ModelLayer {
     public class User {
-        public Map map {get; set;}
-        public Upgrades upgrades {get; set;}
-        public List<Group> groups {get; set;}
-        public int ID {get; set;}
-        public String Name {get; set;}
-        public String Username {get; set;}
-        public String Password {get; set;}
-        public String Email {get; set;}
-        public int Age {get; set;}
-        public String Country {get; set;}
-        public int Ranking {get; set;}
-        public int Level {get; set;}
-        private String Session {get; set;}
+        public Map map { get; set; }
+        public Upgrades upgrades {
+            get;
+            set;
+        }
+        public List<Group> groups {
+            get;
+            set;
+        }
+        public int ID {
+            get;
+            set;
+        }
+        public String Name {
+            get;
+            set;
+        }
+        public String Username {
+            get;
+            set;
+        }
+        public String Password {
+            get;
+            set;
+        }
+        public String Email {
+            get;
+            set;
+        }
+        public int Age {
+            get;
+            set;
+        }
+        public String Country {
+            get;
+            set;
+        }
+        public int Ranking {
+            get;
+            set;
+        }
+        public int Level {
+            get;
+            set;
+        }
+        private String Session {
+            get;
+            set;
+        }
 
-        public User() {}
+        public User() { }
     }
 }
