14ed8bfadc93e3a6f1dbdc445f3f7beccbf3335a
Simonqwadjke <Simonqwadjke@gmail.com>
2016-04-19 10:08:42 +0200

    model

---
+++ Sem4BackEnd/ModelLayer/Buildings/Building.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ModelLayer.Buildings {
+    public class Building {
+        private Building() {
+
+        }
+        public int HitPoints {
+            get;
+            set;
+        }
+        public int Damage {
+            get;
+            set;
+        }
+        public int Armor {
+            get;
+            set;
+        }
+        public double Radius {
+            get;
+            set;
+        }
+        public double Speed {
+            get;
+            set;
+        }
+        public int UnitSize {
+            get;
+            set;
+        }
+        public Point location {
+            get;
+            set;
+        }
+
+        public Size size {
+            get;
+            set;
+        }
+
+        public bool checkLocation(Point location) {
+            if(location.X >= this.location.X && location.X <= this.location.X + size.Width && 
+               location.Y >= this.location.Y && location.Y <= this.location.Y + size.Height) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- Sem4BackEnd/ModelLayer/Map.cs
+++ Sem4BackEnd/ModelLayer/Map.cs
@@ -3,8 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ModelLayer.Buildings;
+using ModelLayer.Units;
+using System.Drawing;
 
 namespace ModelLayer{
     public class Map {
+        private List<Building> buildings;
+        private List<Unit> units;
+
+        public int owner {
+            get;
+            private set;
+        }
+
+        public Map(int owner, List<Building> buildings) {
+            this.buildings = buildings;
+            this.owner = owner;
+        }
+
+        public bool placeUnit(Group group, int x, int y) {
+            return true; //if placement is valid;
+        }
+
+        public bool checkForBuilding(Point location) {
+            bool value = false;
+            foreach(Building building in buildings) {
+                if(building.checkLocation(location)) {
+                    value =  true;
+                }
+            }
+            return value;
+        }
     }
 }
