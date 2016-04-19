f6eda14521b2b9864ea8c2f26206ca360d05db5d
Simonqwadjke <Simonqwadjke@gmail.com>
2016-04-19 17:22:47 +0200

    work on host and applicationserver

---
+++ Sem4BackEnd/ApplicationServer/SessionMannager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ModelLayer;
+
+
+namespace ApplicationServer {
+    public class SessionMannager {
+        List<Session> sessions;
+        Timer cleaner;
+        Random r;
+
+        public SessionMannager() {
+            sessions = new List<Session>();
+            r = new Random();
+            cleaner = new Timer();
+            cleaner.Tick += new EventHandler(cleanUp);
+            cleaner.Interval = TimeSpan.FromHours(1).Milliseconds;
+            cleaner.Start();
+        }
+        public String createSession() {
+            String resource = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            String sessionString;
+            bool result;
+
+            do {
+                result = true;
+                sessionString = "";
+
+                for(int i = 0; i < 10; i++) { //create new session string
+                    sessionString += resource[r.Next(61)];
+                }
+
+                foreach(Session ses in sessions) { //test for duplicate
+                    if(ses.SessionString.Equals(sessionString)) result = false;
+                }
+            } while(!result);
+
+            Session session = new Session(sessionString, DateTime.Now);
+            sessions.Add(session);
+
+            return sessionString;
+        }
+        public bool checkSession(String sessionString) {
+            //baseline
+            bool result = false; 
+            DateTime now = DateTime.Now;
+
+            foreach(Session session in sessions) {
+                if(session.SessionString.Equals(sessionString)) {//if session string is found
+                    if(session.Expired - now < TimeSpan.FromMinutes(15)) {//if session is stil valid
+                        session.Expired = now;//update session time
+                        result = true; //return valid session
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+        public void cleanUp(object sender, EventArgs args){
+            DateTime now = DateTime.Now;
+            for(int i = 0; i < sessions.Count; i++ ) {
+                if(sessions[1].Expired - now > TimeSpan.FromMinutes(15)){
+                    sessions.RemoveAt(i);
+                }
+            }
+        }
+    }
+}
--- Sem4BackEnd/Host/Program.cs
+++ Sem4BackEnd/Host/Program.cs
@@ -3,23 +3,105 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ApplicationServer;
 
 #region Temp
 using ModelLayer;
 using System.Threading;
+using System.Windows.Forms;
 #endregion
 
 namespace Host {
 
     class Program {
+
+
         static void Main(string[] args) {
             new Program();
         }
 
         private Program() {
-            new Thread(() => new ConvertFromUml().ShowDialog()).Start();
-            Console.ReadLine();
+            ConvertFromUml form = new ConvertFromUml();
+            bool running = true;
+            do {
+                header();
+                String s = Console.ReadLine();
+                switch(s) {
+                    case "parser":
+                        new Thread(() => Application.Run(new ConvertFromUml())).Start();
+
+                        break;
+                    case "close threads":
+                        Application.Exit();
+                        break;
+                    case "exit":
+                        Application.Exit();
+                        running = false;
+                        break;
+                    case "user":
+                        createUser();
+                        break;
+                }
+            } while(running);
         }
 
+        private void header() {
+            Console.Clear();
+            Console.WriteLine("########################################################");
+            Console.WriteLine("possible inputs:");
+            Console.WriteLine("parser");
+            Console.WriteLine("close threads");
+            Console.WriteLine("exit");
+            Console.WriteLine("########################################################");
+        }
+
+        private void userHeader() {
+            Console.Clear();
+            Console.WriteLine("########################################################");
+            Console.WriteLine("");
+            Console.WriteLine("########################################################");
+        }
+
+        private void createUser() {
+            User user = new User(new SessionMannager().createSession());
+            user.UserID = 1;
+            bool running = true;
+            do {
+                String s = Console.ReadLine();
+                String firstword = s.Split()[0];
+                switch(firstword.ToLower()) {
+                    case "name":
+                        if(s.Split()[1] == "=" && s.Split().Length == 3) {
+                            user.Name = s.Split()[2];
+                        }
+                        break;
+                    case "username":
+                        if(s.Split()[1] == "=" && s.Split().Length == 3) {
+                            user.Username = s.Split()[2];
+                        }
+                        break;
+                    case "password":
+                        if(s.Split()[1] == "=" && s.Split().Length == 3) {
+                            user.Password = s.Split()[2];
+                        }
+                        break;
+                    case "email":
+                        if(s.Split()[1] == "=" && s.Split().Length == 3) { }
+                        break;
+                    case "birthdate":
+                        if(s.Split()[1] == "=" && s.Split().Length == 3) { }
+                        break;
+                    case "country":
+                        if(s.Split()[1] == "=" && s.Split().Length == 3) { }
+                        break;
+                    case "ranking":
+                        if(s.Split()[1] == "=" && s.Split().Length == 3) { }
+                        break;
+                    case "level":
+                        if(s.Split()[1] == "=" && s.Split().Length == 3) { }
+                        break;
+                }
+            } while(running);
+        }
     }
 }
