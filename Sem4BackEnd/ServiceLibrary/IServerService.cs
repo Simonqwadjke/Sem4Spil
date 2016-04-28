9a87a631f1ceef234c3b03f1cdaf47232b86f7f5
Grilaznar <martin.soe.kristensen@mvb.net>
2016-04-28 02:12:31 +0200

    ServiceLibrary progress

--- Sem4BackEnd/ServiceLibrary/IServerService.cs
+++ Sem4BackEnd/ServiceLibrary/IServerService.cs
@@ -5,6 +5,7 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using ModelLayer;
 
 namespace ServiceLibrary {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
@@ -12,31 +13,7 @@ namespace ServiceLibrary {
     public interface IServerService {
 
         [OperationContract]
-        string GetData(int value);
+        User ReturnNumber();
 
-        [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
-
-        // TODO: Add your service operations here
-    }
-
-
-    // Use a data contract as illustrated in the sample below to add composite types to service operations.
-    [DataContract]
-    public class CompositeType {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
     }
 }
