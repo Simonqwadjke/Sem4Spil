82091dcc4705cdd32c44b6ab0f3e67f6c69dbfd7
Grilaznar <martin.soe.kristensen@mvb.net>
2016-05-08 16:07:53 +0200

    test fra VS

---
+++ Sem4BackEnd/ApplicationServer/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using DataAccessLayer;
+using ModelLayer;
+
+namespace ApplicationServer
+{
+    public class UserManager
+    {
+        public User Login(User user)
+        {
+            MD5 md5 = MD5.Create();
+            IDBUser dbuser = new DBUser();
+            Byte[] inputBytes = Encoding.ASCII.GetBytes(user.Password);
+            Byte[] hash = md5.ComputeHash(inputBytes);
+            user.Password = hash.ToString();
+            return dbuser.Login(user.Username, user.Password);
+        }
+    }
+}
