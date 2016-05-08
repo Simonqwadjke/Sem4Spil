d6bc46b8385390e479bd911bce06a98e8565e04b
Grilaznar <martin.soe.kristensen@mvb.net>
2016-05-08 15:12:14 +0200

    stuff
    Login progress

--- Sem4BackEnd/ServiceLibrary/ServerService.svc.cs
+++ Sem4BackEnd/ServiceLibrary/ServerService.svc.cs
@@ -5,17 +5,24 @@ using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using ApplicationServer;
 using ModelLayer;
 
 namespace ServiceLibrary {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
     public class ServerService : IServerService {
-
+        
         public User ReturnNumber()
         {
             Console.WriteLine("Success!");
             return new User("stuff");
+        }
+
+        public User Login(User user)
+        {
+            UserManager usermgr = new UserManager();
+            return usermgr.Login(user);
         }
     }
 }
