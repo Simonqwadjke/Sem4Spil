7043ec5bc69fbc61ed406d4217927946ec12400d
Grilaznar <martin.soe.kristensen@mvb.net>
2016-05-17 12:40:13 +0200

    database should be done

--- DB-Script/CreateTable.sql
+++ DB-Script/CreateTable.sql
@@ -4,8 +4,8 @@ create table UserData
 (
 UserID		int NOT NULL,
 Name		varchar(20) NOT NULL,
-Username	varchar(30) NOT NULL,
-Password	varchar(50) NOT NULL,
+Username	varchar(30) unique NOT NULL,
+Password	varchar(50) unique NOT NULL,
 Email		varchar(50) NOT NULL,
 Age			date,
 Country		varchar(20) NOT NULL,
@@ -26,9 +26,9 @@ foreign key (InvaderID) references UserData(UserID)
 
 create table Battles
 (
-BattleID	int NOT NULL,
-DefenderID	int NOT NULL,
-AttackerID	int NOT NULL,
+BattleID		int NOT NULL,
+DefenderID		int NOT NULL,
+AttackerID		int NOT NULL,
 BattleOutcome	varchar(10) NOT NULL,
 PlunderedWood	int NOT NULL,
 PlunderedIron	int NOT NULL,
@@ -49,3 +49,31 @@ Primary key (UserID),
 foreign key (UserID) references UserData(UserID)
 );
 
+create table Buildings
+(
+BuildingID	int NOT NULL,
+UserID		int NOT NULL,
+Xlocation	int NOT NULL,
+Yloaction	int NOT NULL,
+type		varchar(20),
+level		int NOT NULL,
+primary key (BuildingID),
+foreign key (UserID) references UserData(UserID)
+);
+
+create table Groups
+(
+UserID		int NOT NULL,
+GroupID		int Unique NOT NULL,
+Primary key (UserID),
+Foreign key (UserID) references UserData(UserID)
+);
+
+create table Units
+(
+UnitID		int NOT NULL,
+GroupID		int NOT NULL,
+type		varchar(20) NOT NULL,
+primary key (UnitID),
+foreign key (GroupID) references Groups(GroupID)
+);
