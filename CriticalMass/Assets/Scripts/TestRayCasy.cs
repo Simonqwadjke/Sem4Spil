f3e48212900e03d072f13ce9ecd5c7d5ddecbb3b
Simonqwadjke <Simonqwadjke@gmail.com>
2016-05-18 19:00:14 +0200

    update test apk

--- CriticalMass/Assets/Scripts/TestRayCasy.cs
+++ CriticalMass/Assets/Scripts/TestRayCasy.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TestRayCasy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Text text;
+
+    void Update() {
+        if(Input.GetMouseButtonDown(0)) {
+            text.text = "CLICK 0";
+        }
+        else if(Input.GetMouseButtonDown(1)) {
+            text.text = "CLICK 1";
+        }
+    }
 }
