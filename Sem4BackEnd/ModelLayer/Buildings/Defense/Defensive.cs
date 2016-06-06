8b3ef313da1ccd87dd8e9709551a8b0e7615243d
Grilaznar <martin.soe.kristensen@mvb.net>
2016-06-06 11:38:05 +0200

    really

--- Sem4BackEnd/ModelLayer/Buildings/Defense/Defensive.cs
+++ Sem4BackEnd/ModelLayer/Buildings/Defense/Defensive.cs
@@ -10,6 +10,7 @@ namespace ModelLayer.Buildings.Defense {
     [KnownType(typeof(Cannon))]
     [KnownType(typeof(GatlingTurret))]
     [KnownType(typeof(FlameThrower))]
+    [KnownType(typeof(Wall))]
     public abstract class Defensive : Building{
         [DataMember]
         public int Damage {
